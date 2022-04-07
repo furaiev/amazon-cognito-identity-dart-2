963bdc33848efeb6022a116dcd1a48af59638f41
Jared Ezzet Hasson <jaredezzethasson@gmail.com>
2022-04-07 10:30:10 -0600

    implement js BigInt workaround to speed up authenticateUser on flutter web

---
+++ lib/src/big_int_modpow_web.dart
@@ -0,0 +1,18 @@
+@JS()
+library bigint_modpow;
+
+import 'package:js/js.dart';
+
+@JS('jsBigIntModPow')
+external String jsBigIntModPow(String b, String e, String m);
+
+BigInt modPow(BigInt? b, BigInt e, BigInt? m) {
+  if (b != null && m != null) {
+    try {
+      return BigInt.parse(jsBigIntModPow(b.toString(), e.toString(), m.toString()));
+    } on NoSuchMethodError {
+      return b.modPow(e, m);
+    }
+  }
+  return BigInt.one;
+}
